e058d863f63d50dcb63d5c124e3698229a70c7fc
Juny Suh <junysuh@Junyui-MacBookAir.local>
2022-07-06 21:43:26 +0900

    last commit

--- server/app.js
+++ server/app.js
@@ -3,16 +3,18 @@ const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
-// const mybatisMapper = require('mybatis-mapper')
+const morgan = require('morgan')
 
 const dbService = require('./dbService')
 // const PORT = process.env.PORT
 const PORT = 9298
+const logger = morgan("dev")
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended : false }))
 app.use(express.static(`${__dirname}/client`))
+app.use(logger)
 
 
 app.listen(PORT, ()=> console.log(`Server listening on port http://localhost:${PORT} ✔`))
